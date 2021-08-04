9a71e6f509167729d0466fc2e0ddf6c8845f015c
Francisco Cortés <franklyn.bdn@gmail.com>
2021-08-04 11:38:58 +0200

    test: add first example with ES modules

---
+++ es-examples/default-case/a.mjs
@@ -0,0 +1,3 @@
+import x from './b.mjs';
+
+console.log(x);
--- tests/integration.sh
+++ tests/integration.sh
@@ -1,24 +1,37 @@
 #!/bin/sh
-mkdir -p dist/examples
-touch dist/output.js dist/node-output.js
-for entry in "examples"/*
+folders=("examples" "es-examples")
+extensions=("js" "mjs")
+suites=("commonJS" "ES modules")
+
+for i in ${!folders[@]}
 do
-  echo "doing a bundle for ${entry}/a.js..."
-  npm start -- --main=${entry}/a.js --output=./dist/${entry}.js &> /dev/null
-  node dist/${entry}.js >> dist/output.js  2> /dev/null
-  node ${entry}/a.js >> dist/node-output.js  2> /dev/null
+  folder=${folders[$i]}
+  extension=${extensions[$i]}
+  suite=${suites[$i]}
+  echo "hello ${folder} ${extension} ${suite}"
+
+  mkdir -p dist/${folder}
+  touch dist/output.js dist/node-output.js
+
+  for entry in "${folder}"/*
+  do
+    echo "doing a bundle for ${entry}/a.${extension}..."
+    npm start -- --main=${entry}/a.${extension} --output=./dist/${entry}.js &> /dev/null
+    node dist/${entry}.js >> dist/output.js  2> /dev/null
+    node ${entry}/a.${extension} >> dist/node-output.js  2> /dev/null
+  done
+
+  tool_tests=$(md5 -q dist/output.js 2> /dev/null|| md5sum dist/output.js | awk '{ print $1 }' 2> /dev/null)
+  node_tests=$(md5 -q dist/node-output.js 2> /dev/null|| md5sum dist/node-output.js | awk '{ print $1 }' 2> /dev/null)
+
+  if [ "$tool_tests" = "$node_tests" ]; then
+    echo -e "\033[0;32m✔ integration tests for ${suite} passed."
+    rm -rf dist
+  else
+    echo -e "\033[0;31m❌ integration tests ${suite} failed. Compare mini JS bundler output and node output for details.\n\n\n"
+    echo -e "\033[0;31mMini JS Bundler: \n------------\n------------\n$(cat dist/output.js)\n\n\n"
+    echo -e "\033[0;31mnode: \n------------\n------------\n$(cat dist/node-output.js)\n\n\n"
+    rm -rf dist
+    exit 1
+  fi
 done
-
-tool_tests=$(md5 -q dist/output.js 2> /dev/null|| md5sum dist/output.js | awk '{ print $1 }' 2> /dev/null)
-node_tests=$(md5 -q dist/node-output.js 2> /dev/null|| md5sum dist/node-output.js | awk '{ print $1 }' 2> /dev/null)
-
-if [ "$tool_tests" = "$node_tests" ]; then
-  echo -e "\033[0;32m✔ integration tests passed."
-  rm -rf dist
-else
-  echo -e "\033[0;31m❌ integration tests failed. Compare mini JS bundler output and node output for details.\n\n\n"
-  echo -e "\033[0;31mMini JS Bundler: \n------------\n------------\n$(cat dist/output.js)\n\n\n"
-  echo -e "\033[0;31mnode: \n------------\n------------\n$(cat dist/node-output.js)\n\n\n"
-  rm -rf dist
-  exit 1
-fi
